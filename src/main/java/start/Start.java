1fd7bdfcb78dcc63f438045cffeef7882f5981a4
Sorana Ghiorghe <120192682+sorigh@users.noreply.github.com>
2025-03-09 00:27:57 +0200

    Added Start.java

---
+++ src/main/java/start/Start.java
@@ -0,0 +1,18 @@
+package start;
+import presentation.View;
+
+/**
+ *  Rularea programului, incepand cu prima pagina
+ */
+public class Start {
+	/**
+	 * metoda de rulare a programului
+	 * @param args argumente
+	 */
+	public static void main(String[] args) {
+
+		new View(null);
+
+    }
+
+}
